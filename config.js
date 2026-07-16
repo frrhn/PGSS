b008af0b3e25db1268cf8f057c99896520e484f5
Muhammad Farhan Rafique <160389850+frrhn@users.noreply.github.com>
2026-07-16 15:40:20 +0500

    Update config.js to include PHARMA_CONFIG

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js - MUST BE GITIGNORED
-window.__APP_CONFIG__ = Object.freeze({
+window.PHARMA_CONFIG = Object.freeze({
     SUPABASE_URL: 'https://sxpvroftqiglonpmghjp.supabase.co',
-    SUPABASE_ANON_KEY: 'sb_publishable_922ZnL9I7l_-pkktN19CGw_5V-HlrgY', // Rotated key!
+    SUPABASE_ANON_KEY: 'sb_publishable_922ZnL9I7l_-pkktN19CGw_5V-HlrgY', 
     ENVIRONMENT: 'production',
     MAX_IMAGE_WIDTH: 1080,
     IMAGE_QUALITY: 0.7,
